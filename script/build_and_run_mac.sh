db4cc1cb1d8a231266608dc4fd61d78a21c94866
Sian Cao <yinshuiboy@gmail.com>
2026-04-29 14:13:38 +0800

    Use logo for mac app icon

--- script/build_and_run_mac.sh
+++ script/build_and_run_mac.sh
@@ -6,6 +6,7 @@ APP_NAME="DevCleaner"
 PACKAGE_DIR="$ROOT/macos/DevCleanerMac"
 DIST_DIR="$ROOT/dist"
 BUNDLE="$DIST_DIR/$APP_NAME.app"
+LOGO_PNG="$ROOT/logo.png"
 VERIFY=0
 BUILD_ONLY=0
 
@@ -31,6 +32,24 @@ mkdir -p "$BUNDLE/Contents/MacOS" "$BUNDLE/Contents/Resources"
 cp "$SWIFT_BIN" "$BUNDLE/Contents/MacOS/$APP_NAME"
 cp "$HELPER_BIN" "$BUNDLE/Contents/Resources/dev-cleaner-helper"
 
+if [[ -f "$LOGO_PNG" ]]; then
+  ICONSET="$DIST_DIR/AppIcon.iconset"
+  rm -rf "$ICONSET"
+  mkdir -p "$ICONSET"
+  sips -z 16 16 "$LOGO_PNG" --out "$ICONSET/icon_16x16.png" >/dev/null
+  sips -z 32 32 "$LOGO_PNG" --out "$ICONSET/icon_16x16@2x.png" >/dev/null
+  sips -z 32 32 "$LOGO_PNG" --out "$ICONSET/icon_32x32.png" >/dev/null
+  sips -z 64 64 "$LOGO_PNG" --out "$ICONSET/icon_32x32@2x.png" >/dev/null
+  sips -z 128 128 "$LOGO_PNG" --out "$ICONSET/icon_128x128.png" >/dev/null
+  sips -z 256 256 "$LOGO_PNG" --out "$ICONSET/icon_128x128@2x.png" >/dev/null
+  sips -z 256 256 "$LOGO_PNG" --out "$ICONSET/icon_256x256.png" >/dev/null
+  sips -z 512 512 "$LOGO_PNG" --out "$ICONSET/icon_256x256@2x.png" >/dev/null
+  sips -z 512 512 "$LOGO_PNG" --out "$ICONSET/icon_512x512.png" >/dev/null
+  sips -z 1024 1024 "$LOGO_PNG" --out "$ICONSET/icon_512x512@2x.png" >/dev/null
+  iconutil -c icns "$ICONSET" -o "$BUNDLE/Contents/Resources/AppIcon.icns"
+  rm -rf "$ICONSET"
+fi
+
 cat > "$BUNDLE/Contents/Info.plist" <<PLIST
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -42,6 +61,8 @@ cat > "$BUNDLE/Contents/Info.plist" <<PLIST
   <string>com.sian.devcleaner</string>
   <key>CFBundleName</key>
   <string>Dev Cleaner</string>
+  <key>CFBundleIconFile</key>
+  <string>AppIcon</string>
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
